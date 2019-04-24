b2d33922ca1bb725ea2f607e4224c40da9fa2586
srikantkota <49935511+srikantkota@users.noreply.github.com>
2019-04-25 07:48:52 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,14 @@ const express = require('express');
 const verifyJwt = require(Path.join(__dirname, 'lib', 'jwt.js'));
 // Helper class that handles all the interactions with Salesforce Service Cloud
 // and makes sure open connections are reused for subsequent requests.
-
+var bodyParser  = require('body-parser');
 const app = express();
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 // Register middleware that parses the request payload.
 /*app.use(require('body-parser').raw({
 	type: 'application/jwt'
 })); */
-
+app.use(bodyParser.raw({type: 'application/jwt'}));
 // Route that is called for every contact who reaches the custom split activity
 app.post('/activity/execute', (req, res) => {
 		//verifyJwt(req.body, Pkg.options.salesforce.marketingCloud.jwtSecret,(err, decoded) => {

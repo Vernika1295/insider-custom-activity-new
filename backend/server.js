51c5eb43c0afecd96d124b71241945760b1ff0da
srikantkota <49935511+srikantkota@users.noreply.github.com>
2019-04-25 12:08:42 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.post('/activity/execute', (req, res) => {
 		// Object representing the data in the JWT
 		var decoded = jwt.decode(req);
 		console.log("Decoded after JWT: "+JSON.stringify(decoded));
-		console.log("decode length"+decode.inArguments.length);
+		console.log("decoded length"+decoded.inArguments.length);
 		if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
 			console.log("decoded->"+decoded);
 			var apikey = decoded.inArguments[0]["api_key"];
